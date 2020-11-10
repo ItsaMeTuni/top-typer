a02d4561fd8971fd9e645793f2cff2df2c2e065f
Lucas Antunes <lucasba8@gmail.com>
2020-11-09 23:43:24 -0300

    create checkbox, user can now see many curves at same time in stats

---
+++ src/components/checkbox.svelte
@@ -0,0 +1,64 @@
+<script lang="typescript">
+
+import { mdiCheckboxMarkedCircleOutline, mdiCheckboxBlankCircleOutline } from '@mdi/js'
+
+export let checked = false;
+export let label = 'Test';
+export let color = 'white';
+export let invert = false;
+
+let _checked: boolean;
+$: _checked = checked === !invert;
+
+let checkPath: string;
+$: checkPath = _checked ? mdiCheckboxMarkedCircleOutline : mdiCheckboxBlankCircleOutline;
+
+function toggle()
+{
+    checked = !checked;
+}
+
+</script>
+
+<template>
+    <div
+        class="checkbox"
+        on:click={toggle}
+    >
+        <svg style={`--color: ${color};`}>
+            <path d={checkPath}></path>
+        </svg> 
+        <span class="text">{label}</span>
+    </div>
+</template>
+
+<style lang="scss">
+
+.checkbox
+{
+    margin: 16px 0;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    cursor: pointer;
+    user-select: none;
+}
+
+svg
+{
+    margin-right: 10px;
+
+    display: block;
+
+    width: 1em;
+    height: 1em;
+
+    path
+    {
+        fill: var(--color);
+    }
+}
+
+</style>
--- src/views/stats.svelte
+++ src/views/stats.svelte
@@ -1,11 +1,8 @@
 <script lang="typescript">
-import Layout from "../layout.svelte";
 import { getStats } from "../statManager";
 import {Line} from "svelte-chartjs"
-import Select from "../components/select.svelte";
-import Logo from "../components/logo.svelte";
-import Nav from "../components/nav.svelte";
 import Header from "../components/header.svelte";
+import Checkbox from "../components/checkbox.svelte";
 
 const stats = getStats();
 const calculatedStats = stats.map(x => x.calculateStats());
@@ -14,84 +11,106 @@ const accuracyHistory = calculatedStats.map(x => Math.floor(x.keystrokeAccuracy 
 const pace = calculatedStats.map(x => Math.floor(x.pace * 100));
 const dates = stats.map(x => new Date(x.timestamp));
 
+interface Dataset
+{
+    label: string;
+    data: {x: Date, y: number}[];
+    fill: boolean;
+    borderColor: string;
+    pointBackgroundColor: string;
+    pointRadius: number;
+    pointHoverRadius: number;
+    yAxisID: string;
+    hidden: boolean;
+}
 
+interface Axis
+{
+    id: string;
+    type: 'linear';
+    ticks: {
+        suggestedMin: number;
+        suggestedMax: number;
+    },
+    display: 'auto',
+    weight: number,
+}
 
-function createData(label: string, color: string, data: number[]): any
+function makeLine(label: string, color: string, data: number[], min: number, max: number, initialVisiblity: boolean, axisWeight: number): [Dataset, Axis]
+{
+    const dataset: Dataset = {
+        label,
+        data: data.map((y, i) => ({x: dates[i], y})),
+        fill: false,
+        borderColor: color,
+        pointBackgroundColor: color,
+        pointRadius: 6,
+        pointHoverRadius: 8,
+        yAxisID: label,
+        hidden: !initialVisiblity,
+    };
+
+    const yAxis: Axis = {
+        id: label,
+        type: 'linear',
+        display: 'auto',
+        weight: 1 / axisWeight,
+        ticks: {
+            suggestedMin: min,
+            suggestedMax: max,
+        },
+    };
+
+    return [dataset, yAxis];
+}
+
+function makeChart(datasetsAndAxes: [Dataset, Axis][]): any
 {
     return {
-        datasets: [
-            {
-                label,
-                data: data.map((y, i) => ({x: dates[i], y})),
-                fill: false,
-                borderColor: color,
-                pointBackgroundColor: color,
-                pointRadius: 6,
-                pointHoverRadius: 8,
+        data: {
+            datasets: datasetsAndAxes.map(x => x[0]),
+        },
+        options: {
+            legend: {
+                display: false,
             },
-        ],
-    };
-}
-
-function createOptions(min: number, max: number)
-{
-    return {
-        scales: {
-            yAxes: [
-                {
-                    type: 'linear',
-                    ticks: {
-                        suggestedMin: min,
-                        suggestedMax: max,
+            animation: {
+                duration: 0,
+            },
+            scales: {
+                yAxes: datasetsAndAxes.map(x => x[1]),
+                xAxes: [
+                    {
+                        type: 'time',
+                        distribution: 'series',
                     },
-                },
-            ],
-            xAxes: [
-                {
-                    type: 'time',
-                    distribution: 'series',
-                },
-            ],
+                ],
+            },
         },
-    };
+    }
 }
 
-const charts = [
-    {
-        text: 'WPM',
-        value: {
-            data: createData('WPM', '#27D239', wpmHistory),
-            options: createOptions(0, 130),
-        },
-    },
-    {
-        text: 'Accuracy',
-        value: {
-            data: createData('Accuracy', '#6892FF', accuracyHistory),
-            options: createOptions(40, 100),
-        },
-    },
-    {
-        text: 'Pace',
-        value: {
-            data: createData('Pace', '#FF66C7', pace),
-            options: createOptions(20, 100),
-        },
-    },
-]
+let chart = makeChart([
+    makeLine('WPM', '#27D239', wpmHistory, 0, 130, true, 3),
+    makeLine('Accuracy', '#6892FF', accuracyHistory, 40, 100, false, 2),
+    makeLine('Pace', '#FF66C7', pace, 20, 100, false, 1),
+]);
 
-let selectedChart = charts[0];
 
 </script>
 
 <template>
     <div class="layout">
         <Header></Header>
-        <h1>Stats</h1>
+        <h1 class="title">Your stats</h1>
         <div class="chart-wrapper">
-            <Line data={selectedChart.value.data} options={selectedChart.value.options}></Line>
+            <Line data={chart.data} options={chart.options}></Line>
         </div>
-        <Select options={charts} bind:selected={selectedChart}></Select>
+        <div class="checkboxes">
+            {#each chart.data.datasets as dataset}
+                <Checkbox bind:checked={dataset.hidden} label={dataset.label} color={dataset.borderColor} invert={true}></Checkbox>
+            {/each}
+        </div>
     </div>
 </template>
 
@@ -104,9 +123,20 @@ let selectedChart = charts[0];
     align-items: center;
 }
 
+.title
+{
+    margin: 1em 0;
+    font-weight: 400;
+}
+
 .chart-wrapper
 {
     width: 50vw;
+}
+
+.checkboxes
+{
+    margin-top: 32px;
 }
 
 </style>
